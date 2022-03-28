1be4608353664fa387df98317e66010ecb78e768
abishekpepz <abishekpepz@gmail.com>
2022-03-28 14:25:07 +0530

    here

---
+++ src/org/com/GitSample.java
@@ -0,0 +1,15 @@
+package org.com;
+
+public class GitSample {
+	public void method1() {
+	       System.out.println("pushing");
+	}
+		
+		
+		public static void main(String[] args) {
+			GitSample g = new GitSample();
+			g.method1();
+		}
+		
+	
+}
